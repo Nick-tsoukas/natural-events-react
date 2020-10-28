63b698a10dfe55b6a193114af3d70666f38048f8
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-10-28 17:33:43 -0500

    Adding projects

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+/* src/App.js */
+import React, {
+  useRef,
+  useEffect,
+  useState
+} from 'react';
+import axios from 'axios';
+import ReactMapboxGl, { Layer, Feature, GeoJSONLayer, Marker } from 'react-mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import './App.css';
+import markerImage from './room-white-18dp.svg';
+
+const Map = ReactMapboxGl({
+  accessToken:
+  process.env.REACT_APP_MAPBOX_ACCESS_TOKEN
+});
 
 function App() {
+  
+  const [events, setEvents] = useState({
+    plots: []
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://eonet.sci.gsfc.nasa.gov/api/v3/events',
+      );
+      setEvents({
+        plots: result.data.events
+      });
+      console.log(result.data.events)
+    };
+    fetchData();
+
+  },[]);
+  
+  
+    
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Map
+      style="mapbox://styles/mapbox/satellite-v9"
+      containerStyle={{
+       height: '100vh',
+       width: '100vw'
+      }}
+    >
+      
+      {events.plots.map((e) => {
+      if(e.geometry[0].coordinates.length === 2) {
+        return (
+          <Marker
+          coordinates={e.geometry[0].coordinates}
+          anchor="bottom">
+        <img style={{color: 'red'}} src={markerImage}/>
+        </Marker>
+        )
+      }
+      })}
+    </Map>
+    );
 }
 
 export default App;
